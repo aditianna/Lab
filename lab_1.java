fa6b8e7ca4c1ce629e34ac3d6a97c8bbe08612ab
Aditi Anna <aditianna@gmail.com>
2019-12-03 09:21:48 -0500

    changed to next semester

--- lab_1.java
+++ lab_1.java
@@ -1,6 +1,6 @@
 public class lab_1 {
     public static void main (String args[])
     {
-        System.out.println("Hello World");
+        System.out.println("Hello to the next Semester");
     }
 }
